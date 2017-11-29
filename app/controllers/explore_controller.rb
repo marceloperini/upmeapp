06106f2450864cece3e45ac27f50d779429b4e97
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2017-11-28 22:43:00 -0200

    fixing interests

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,14 +2,20 @@ class ExploreController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @all_areas = AreaDeInteresse.where(name: ["Engenharias", "Marketing", "Administração"])
+    @all_areas = AreaDeInteresse.where(name: interest_areas)
 
     if params and params[:areas_interesse].present?
       @selected_areas = AreaDeInteresse.where(id: params[:areas_interesse].map { |item| item.to_i})
     else
-      @selected_areas = AreaDeInteresse.where(name: ["Engenharias", "Marketing", "Administração"])
+      @selected_areas = AreaDeInteresse.where(name: interest_areas)
     end
 
     @avaliable_projects = current_user.available_projects
+  end
+
+  private
+
+  def interest_areas
+    %w(Tecnologia Engenharias Marketing Administração)
   end
 end
